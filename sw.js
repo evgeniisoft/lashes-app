4be791c39a5a6766c83e12efed5adc960eabf4bf
evgeniisoft <evgenijswg@gmail.com>
2026-08-30 18:51:42 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,3 +22,41 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+// Автоматическое обновление
+self.addEventListener('install', event => {
+  self.skipWaiting(); // Немедленно активировать новый SW
+});
+
+self.addEventListener('activate', event => {
+  // Удаляем старый кэш
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Стратегия: сначала сеть, потом кэш
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Кэшируем свежий ответ
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        // Если сеть недоступна - берем из кэша
+        return caches.match(event.request);
+      })
+  );
+});
